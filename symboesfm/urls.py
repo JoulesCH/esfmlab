35927dfe6e08035b4cd8aa1ca7f7befd3ff8de12
Julio César <wordls@yahoo.es>
2021-04-29 13:42:50 -0500

    import static

--- symboesfm/urls.py
+++ symboesfm/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from symboesfm import views as home_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
