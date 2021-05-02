a32878c7e76e1fb935fd96e7a92ead7f0dcfbf91
Julio César <wordls@yahoo.es>
2021-05-01 19:21:24 -0500

    esfmlab

--- integracion/views.py
+++ integracion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from sympy import parse_expr, latex
 from symboesfm.metodos import integracion_numerica
 def view(request):
